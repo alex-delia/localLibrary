a085d1de9324e5dc6b0d40735a813e17afd53159
alex-delia <adelia3@uwo.ca>
2024-06-13 16:49:02 -0400

    create author list page

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -22,7 +23,7 @@ const AuthorSchema = new Schema({
 });
 
 //virtual for author's full name
-AuthorSchema.virtual('name').get(() => {
+AuthorSchema.virtual('name').get(function () {
     // To avoid errors in cases where an author does not have either a family name or first name
     // We want to make sure we handle the exception by returning an empty string for that case
     let fullname = '';
@@ -37,6 +38,14 @@ AuthorSchema.virtual('name').get(() => {
 //Virtual for author's URL
 AuthorSchema.virtual('url').get(function () {
     return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+    return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+    return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 //export module
