afc9c16990153f9039a6fdb48a73f8fb5a9484c2
alex-delia <adelia3@uwo.ca>
2024-06-14 00:48:26 -0400

    create error when trying to update invalid genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -129,6 +129,14 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
     const genre = await Genre.findById(req.params.id);
+
+    if (genre === null) {
+        // No results.
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+    }
+
     res.render('genre_form', { title: 'Update Genre', genre: genre });
 });
 
